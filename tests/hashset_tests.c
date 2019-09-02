e8a8b668f730a2c2829cd850b9927e094d1f7ec7
Kyle Glaws <kyle.glaws@gmail.com>
2019-09-01 22:05:25 -0400

    finish hashset tests.

--- tests/hashset_tests.c
+++ tests/hashset_tests.c
@@ -16,9 +16,9 @@ static int hashset_new_tests()
   hashset* hs = hashset_new(100, 0x1234);
   if (hs->seed != 0x1234)
   {
-    printf("TEST 1: hashset contains incorrect seed value: %d. Expected: %d.\n", hs->seed, 0x1234);
+    printf("TEST 1: hashset contains incorrect seed value: %llu. Expected: %llu.\n", hs->seed, (unsigned long long) 0x1234);
     retval = -1;
-  {
+  }
   if (hs->num_buckets != 100)
   {
     printf("TEST 1: hashset contains incorrect num_buckets value %d. Expected %d.\n", hs->num_buckets, 0x1234);
@@ -32,13 +32,78 @@ static int hashset_new_tests()
 
 static int hashset_add_tests()
 {
+  int retval = 0;
+
+  /* Test 1 */
+  hashset* hs = hashset_new(100, 0x1234);
+  if (hashset_add(hs, datacont_new("A", CHAR, 1)) != 0)
+  {
+    printf("TEST 1: hashset_add() should return 0 when adding a new item.\n");
+    retval = -1;
+  }
+
+  /* Test 2 */
+  if (hashset_add(hs, datacont_new("A", CHAR, 1)) != 1)
+  {
+    printf("TEST 2: hashset_add() should return 1 when trying to add an already present item.\n");
+    retval = -1;
+  }
+  hashset_delete(hs);
+
+  return retval;
+}
+
+
+static int hashset_remove_tests()
+{
+  int retval = 0;
+  
+  /* not yet implemented */
+
+  return retval;
+}
+
+
+static int hashset_contains_tests()
+{
+  int retval = 0;
+
+  /* Test 1 */
+  hashset* hs = hashset_new(100, 0x1234);
+  datacont* dc = datacont_new("A", CHAR, 1);
+  hashset_add(hs, dc);
+  if (hashset_contains(hs, dc) == 0)
+  {
+    printf("TEST 1: hashset_contains() should return 1.\n");
+    retval = -1;
+  }
+  hashset_delete(hs);
+
+  return retval;
+}
 
 
 int main()
 {
   int retval = -1;
 
-  
+  printf("==-----------------------------------==\n");
+  printf("Running hashset_new_tests()...\n");
+  if (hashset_new_tests()) retval = -1;
+  printf("done.\n");
+  printf("==-----------------------------------==\n\n");
+
+  printf("==-----------------------------------==\n");
+  printf("Running hashset_add_tests()...\n");
+  if (hashset_add_tests()) retval = -1;
+  printf("done.\n");
+  printf("==-----------------------------------==\n\n");
+
+  printf("==-----------------------------------==\n");
+  printf("Running hashset_contains_tests()...\n");
+  if (hashset_contains_tests()) retval = -1;
+  printf("done.\n");
+  printf("==-----------------------------------==\n\n");
 
   return retval;
 }
