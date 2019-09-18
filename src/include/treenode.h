35243605e95b6b0f623a41c9b3f449e48a69b4a2
Kyle Glaws <kyle.glaws@gmail.com>
2019-09-18 16:05:57 -0400

    add documentation

--- src/include/treenode.h
+++ src/include/treenode.h
@@ -17,6 +17,32 @@ void treenode_delete(treenode* tn);
 
 int treenode_add(treenode* tn, const datacont* dc);
 
+/* ---------------------------------
+ * treenode_remove():
+ * Remove a node contained within the
+ * treenode* parameter that matches the
+ * data container parameter. If you call
+ * this function directly (i.e. not through
+ * treeset_remove()), you are expected to
+ * keep track of the number of remaining
+ * nodes on your own. If you remove the last
+ * node from the tree, you will have an
+ * invalid root pointer. The only clue you
+ * will get is a return value of 0 upon
+ * successful removal, at which point you
+ * can subtract 1 from your own counter if
+ * you have one.
+ *
+ * inputs:
+ * treenode* tn - remove treenode from this
+ * root node.
+ * datacont* dc - remove treenode that contains
+ * the same value as this data container
+ *
+ * returns:
+ * 0 - successfully removed node
+ * 1 - node not found
+ */
 int treenode_remove(treenode* tn, const datacont* dc);
 
 int treenode_contains(const treenode* tn, const datacont* dc);
@@ -25,4 +51,7 @@ static int _treenode_height(const treenode* tn, unsigned int accum);
 
 unsigned int treenode_height(const treenode* tn);
 
+unsigned int treenode_size(const treenode* tn);
+
 #endif
+
--- src/include/treeset.h
+++ src/include/treeset.h
@@ -16,6 +16,23 @@ void treeset_delete(treeset* ts);
 
 int treeset_add(treeset* ts, const datacont* dc);
 
+/* ------------------------------
+ * treeset_remove():
+ * Remove a node from a treeset that contains a
+ * data container matching the value of the 'dc'
+ * argument. On successful removal, 1 is subtracted 
+ * from the treeset's num_nodes value. If num_nodes 
+ * reaches 0, the treeset's root pointer is set to NULL.
+ *
+ * inputs:
+ * treeset* ts - remove treenode from this treeset.
+ * datacont* dc - remove treenode with matching 
+ * datacont value.
+ *
+ * returns:
+ * 0 - successfully removed node
+ * 1 - node not found
+ */
 int treeset_remove(treeset* ts, const datacont* dc);
 
 int treeset_contains(const treeset* ts, const datacont* dc);
@@ -23,3 +40,4 @@ int treeset_contains(const treeset* ts, const datacont* dc);
 unsigned int treeset_height(const treeset* ts);
 
 #endif
+
