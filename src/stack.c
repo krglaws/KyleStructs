2f8f6d9ee5063bf004df381ca628f6629b2f5467
Kyle Glaws <kyle.glaws@gmail.com>
2019-11-03 13:37:07 -0500

    finish stack implementation

--- src/stack.c
+++ src/stack.c
@@ -1,5 +1,42 @@
 
+#include <stdlib.h>
+
 #include "include/datacont.h"
 #include "include/listnode.h"
 #include "include/stack.h"
+
+
+stack* stack_new()
+{
+  return calloc(1, sizeof(stack));  
+}
+
+
+void stack_delete(stack* s)
+{
+  listnode_delete_all(s->head);
+  free(s);
+}
+
+
+int stack_push(stack* s, const datacont* dc)
+{
+  if (s->head) return listnode_insert(&(s->head), dc, 0);
+  s->head = listnode_new(dc);
+  return 0;
+}
+
+
+datacont* stack_pop(stack* s)
+{
+  datacont* dc = listnode_get(s->head, 0);
+  listnode_remove_at(&(s->head), 0);
+  return dc;
+}
+
+
+unsigned int stack_height(stack* s)
+{
+  return listnode_length(s->head);
+}
 
