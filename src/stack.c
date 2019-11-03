39e2c27ba4da6947a8d4a9d06e4d0dd9d025883b
Kyle Glaws <kyle.glaws@gmail.com>
2019-11-03 18:23:09 -0500

    finish stack implementation. add stack tests.

--- src/stack.c
+++ src/stack.c
@@ -14,6 +14,7 @@ stack* stack_new()
 
 void stack_delete(stack* s)
 {
+  if (s == NULL) return;
   listnode_delete_all(s->head);
   free(s);
 }
@@ -21,22 +22,31 @@ void stack_delete(stack* s)
 
 int stack_push(stack* s, const datacont* dc)
 {
+  if (s == NULL) return -1;
+
   if (s->head) return listnode_insert(&(s->head), dc, 0);
+
   s->head = listnode_new(dc);
+
   return 0;
 }
 
 
 datacont* stack_pop(stack* s)
 {
+  if (s == NULL) return NULL;
+
   datacont* dc = listnode_get(s->head, 0);
+
   listnode_remove_at(&(s->head), 0);
+
   return dc;
 }
 
 
 unsigned int stack_height(stack* s)
 {
+  if (s == NULL) return 0;
   return listnode_length(s->head);
 }
 
