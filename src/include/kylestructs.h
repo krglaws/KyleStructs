9c38554791cfa3163f38acdb9a76fa2161f1725a
Kyle Glaws <kyle.glaws@gmail.com>
2019-11-15 11:34:34 -0500

    create universal kylestructs header

---
+++ src/include/kylestructs.h
@@ -0,0 +1,30 @@
+
+/* main header */
+
+#ifndef _KYLESTRUCTS_H_
+#define _KYLESTRUCTS_H_
+
+#include <kylestructs/datacont.h>
+
+#include <kylestructs/listnode.h>
+
+#include <kylestructs/treesetnode.h>
+
+#include <kylestructs/treemapnode.h>
+
+#include <kylestructs/list.h>
+
+#include <kylestructs/stack.h>
+
+#include <kylestructs/queue.h>
+
+#include <kylestructs/treeset.h>
+
+#include <kylestructs/treemap.h>
+
+#include <kylestructs/hashset.h>
+
+#include <kylestructs/hashmap.h>
+
+#endif
+
